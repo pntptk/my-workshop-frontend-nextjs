ea10fca56cd2b7143c4042e5ed8999992ef06917
Pladniti Phomtakai <167268137+ITiti123npc1npg@users.noreply.github.com>
2026-03-14 23:53:38 +0700

    add

--- app/expenses/page.tsx
+++ app/expenses/page.tsx
@@ -16,12 +16,12 @@ export default function ExpensesPage() {
   const [title, setTitle] = useState<string>("");
   const [amount, setAmount] = useState<number>();
   const [category, setCategory] = useState<string>();
-  const [date, setDate] = useState<string>(new Date().toISOString().split('T')[0]);
+  const [date, setDate] = useState<string>(
+    new Date().toISOString().split("T")[0],
+  );
   const [type, setType] = useState<"INCOME" | "EXPENSE">();
 
-
-
-  console.log("Date : ",date)
+  console.log("Date : ", date);
   useEffect(() => {
     fetchTransaction();
   }, []);
@@ -39,55 +39,65 @@ export default function ExpensesPage() {
     <div className="bg-gray-400 w-full h-full min-h-screen flex flex-col items-center ">
       <div className="flex flex-col mt-5">
         <h1 className="font-bold text-5xl p-3">Expenses Tracking</h1>
-        <div className="bg-gray-700 p-2">
-          <div className="flex flex-col ">
-            <label htmlFor="">Title</label>
-            <input
-              type="text"
-              className="border border-black-2 rounded-sm p-2 outline-0"
-              onChange={(e)=>setTitle(e.target.value)}
-            />
+        <div className="bg-gray-700 p-5 rounded-2xl">
+          <div className="flex space-x-2">
+            <div className="flex flex-col ">
+              <label htmlFor="">Title</label>
+              <input
+                type="text"
+                className="border border-black-2 rounded-sm p-2 outline-0"
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col ">
+              <label htmlFor="">Amount</label>
+              <input
+                type="number"
+                className="border border-black-2 rounded-sm p-2 outline-0"
+                onChange={(e) => setAmount(Number(e.target.value))}
+              />
+            </div>
           </div>
-          <div className="flex flex-col ">
-            <label htmlFor="">Amount</label>
-            <input
-              type="number"
-              className="border border-black-2 rounded-sm p-2 outline-0"
-              onChange={(e)=>setAmount(Number(e.target.value))}
-            />
-          </div>
-          <div className="flex flex-col ">
-            <label htmlFor="">Type</label>
-            <select
-              name=""
-              id=""
-              value={type}
-              onChange={(e) => setType(e.target.value as any)}
-              className="bg-gray-700 border border-black-2 rounded-sm p-2 outline-0"
-            >
-              <option value="INCOME">INCOME</option>
-              <option value="EXPENSE">EXPENSE</option>
-            </select>
+          <div className="flex ">
+            <div className="flex flex-col ">
+              <label htmlFor="">Category</label>
+              <input
+                type="text"
+                className="border border-black-2 rounded-sm p-2 outline-0"
+                onChange={(e) => setCategory(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="">Type</label>
+              <select
+                name=""
+                id=""
+                value={type}
+                onChange={(e) => setType(e.target.value as any)}
+                className="bg-gray-700 border border-black-2 rounded-sm p-2 outline-0"
+              >
+                <option value="INCOME">INCOME</option>
+                <option value="EXPENSE">EXPENSE</option>
+              </select>
+            </div>
           </div>
 
-          <div className="flex flex-col ">
-            <label htmlFor="">Category</label>
-            <input
-              type="text"
-              className="border border-black-2 rounded-sm p-2 outline-0"
-              onChange={(e)=>setCategory(e.target.value)}
-            />
-          </div>
           <div className="flex flex-col ">
             <label htmlFor="">Date</label>
             <input
               type="date"
               className="border border-black-2 rounded-sm p-2 outline-0"
               value={date}
-              onChange={(e)=>setDate(e.target.value)}
+              onChange={(e) => setDate(e.target.value)}
             />
           </div>
+          <div className="mt-5 flex justify-end">
+            <button className="bg-green-500 rounded-sm px-3 py-2">save</button>
+          </div>
         </div>
+      </div>
+      <div className="">
+        <div className=""></div>
       </div>
     </div>
   );
